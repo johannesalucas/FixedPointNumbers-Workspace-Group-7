a7c524a959c4fdb2a2351c67c7a35fc3f63eb325
rodgersmaighacho <rodgersmaighacho@gmail.com>
2026-04-04 18:20:25 +0800

    Implement power method for fixNum class(Full final update)
    Added power method to handle exponentiation for fixNum.

--- fixpoint_pkg.py
+++ fixpoint_pkg.py
@@ -10,7 +10,8 @@ class fixNum:
     def to_float(self):
         '''Converts the internal representation to a float for calculation'''
         return self.a +(self.b/10**self.precision)
-
+    
+    @staticmethod #Calling the function without an instance
     def from_float(value,precision=2):
         #Creates a fixNum object from a float result
         a=int(value)
@@ -28,7 +29,22 @@ class fixNum:
         
         return fixNum.from_float(raw_result,self.precision)
 
+    def power(self,n): #Bonus task
+        if self.a==0 and self.b==0: #Accounting for when the value is 0 (Undefined)
+            print("Error. Cannot raise 0 to a power")
+            return None
+        if isinstance(n,fixNum): #Accounts for both standard integers and complex fixed-point exponents
+            exponent=n.to_float()
+        else:
+            exponent=n #Assume it is a positive/negative integer
+            
+        result_val=self.to_float()**exponent   
+        return fixNum.from_float(result_val,self.precision)#Return as a new fixed point using the precision:
+                     
+
     def __str__(self):
         return f"{self.a}.{self.b:0{self.precision}d}" #The 0 pads the integers to avoid confusion between 12.05 and 12.5 for example. 
-        
+
+
+
     
